3dc15d6234e7e05d6cd9981293c7da0ad25a5cf3
zkbreeze <zk.kyoto@gmail.com>
2014-12-16 17:31:06 +0900

    revise the converter

--- lib/KVSMLStructuredVolumeReader.cpp
+++ lib/KVSMLStructuredVolumeReader.cpp
@@ -7,6 +7,7 @@
 //
 
 #include "KVSMLStructuredVolumeReader.h"
+#include <vtkInformation.h>
 
 namespace kun
 {
@@ -26,27 +27,23 @@ vtkStructuredPoints* KVSMLStructuredVolumeReader::OutputVTKStructuredVolume()
 {
 	vtkStructuredPoints* structuredPoints = vtkStructuredPoints::New();
 	structuredPoints->SetDimensions( m_volume->resolution().x(), m_volume->resolution().y(), m_volume->resolution().z() );
-	// structuredPoints->SetNumberOfScalarComponents( 1 );
-	// structuredPoints->SetScalarTypeToDouble();
-	
+
 	int* dims = structuredPoints->GetDimensions();
+	structuredPoints->AllocateScalars( VTK_UNSIGNED_CHAR, 1);
 	
 	std::cout << "Dims: " << " x: " << dims[0] << " y: " << dims[1] << " z: " << dims[2] << std::endl;
 	std::cout << "Number of points: " << structuredPoints->GetNumberOfPoints() << std::endl;
 	std::cout << "Number of cells: " << structuredPoints->GetNumberOfCells() << std::endl;
 
 	//fill every entry of the image data with "2.0"
-	const float* pvalue_ori = static_cast<const float*>( m_volume->values().data() );
-	for( size_t k = 0; k < dims[2]; k++ )
-		for( size_t j = 0; j < dims[1]; j++ )
-			for( size_t i = 0; i < dims[0]; i++ )
+	const unsigned char* pvalue_ori = static_cast<const unsigned char*>( m_volume->values().data() );
+	for( int k = 0; k < dims[2]; k++ )
+		for( int j = 0; j < dims[1]; j++ )
+			for( int i = 0; i < dims[0]; i++ )
 			{
 				size_t index = i + j * dims[0] + k * dims[0] * dims[1];
-
-				std::cout << pvalue_ori[index] << std::endl;
-				double* pvalue = static_cast<double*>( structuredPoints->GetScalarPointer(i, j, k) );
-				pvalue[0] = pvalue_ori[index];
-
+				unsigned char* pixel = static_cast<unsigned char*>(structuredPoints->GetScalarPointer(i,j,k));
+				pixel[0] = pvalue_ori[index];
 			}
 
 	std::cout << "Finish to load the KVSML as the vtkStructuredPoints." << std::endl;
